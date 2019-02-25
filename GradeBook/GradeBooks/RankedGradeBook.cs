ec3a09e04f3732822278f1e2c4ae2b97ec266582
dcolburn <47987075+dcolburn@users.noreply.github.com>
2019-02-25 15:33:38 -0500

    End of section 3
    Code upload for end of section 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,44 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            char retVal;
+            int gradeInterval = 0;
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            gradeInterval = Students.Count / 5;
+            List<double> grades = new List<double>();
+            foreach( var student in Students)
+            {
+                grades.Add(student.AverageGrade);
+            }
+            grades.Sort();
+            grades.Reverse();
+            if(averageGrade >= grades[gradeInterval - 1])
+            {
+                retVal = 'A';
+            }
+            else if(averageGrade >= grades[gradeInterval*2-1] )
+            {
+                retVal = 'B';
+            }
+            else if(averageGrade >= grades[gradeInterval*3-1])
+            {
+                retVal = 'C';
+            }
+            else if(averageGrade >= grades[gradeInterval*4-1])
+            {
+                retVal = 'D';
+            }
+            else
+            {
+                retVal = 'F';
+            }
+            return retVal;
+        }
     }
 }
